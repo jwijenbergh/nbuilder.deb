87486fe62e6e4bf7e6ddd898dcae8fac521d0638
François Kooman <fkooman@tuxed.net>
2022-04-11 11:47:47 +0200

    hopefully fix using golang backports on Debian

--- eduvpn_v2-dev.sh
+++ eduvpn_v2-dev.sh
@@ -29,9 +29,8 @@ BASE_PACKAGE_URL_LIST="
 	https://git.sr.ht/~fkooman/php-saml-sp-artwork-eduVPN.deb|main
 "
 
-DEBIAN_PACKAGE_URL_LIST="
+DEBIAN_EXTRA_PACKAGE_URL_LIST="
 	https://salsa.debian.org/php-team/pear/php-constant-time|debian/2.4.0-1
-	${BASE_PACKAGE_URL_LIST}
 "
 
 TMP_DIR=$(mktemp -d)
@@ -40,11 +39,25 @@ for DISTRO_SUITE in ${DISTRO_SUITE_LIST}; do
 	DISTRO=$(echo ${DISTRO_SUITE} | cut -d '|' -f 1)
 	SUITE=$(echo ${DISTRO_SUITE} | cut -d '|' -f 2)
 
+    PACKAGE_URL_LIST=${BASE_PACKAGE_URL_LIST}
+    EXTRA_DEP=""
+
 	if [ "debian" = "${DISTRO}" ]; then
-		PACKAGE_URL_LIST=${DEBIAN_PACKAGE_URL_LIST}
-	else
-		PACKAGE_URL_LIST=${BASE_PACKAGE_URL_LIST}
-	fi
+	    # on Debian we need to include php-constant-time as it is not part of 
+	    # the normal repository
+		PACKAGE_URL_LIST=${DEBIAN_EXTRA_PACKAGE_URL_LIST} ${BASE_PACKAGE_URL_LIST}
+
+		# we want to use the backports of Golang on Debian, I have no idea how
+		# to make this less awkward...
+		if [ "bullseye" = "${SUITE}" ]; then
+            # https://packages.debian.org/bullseye/golang-go
+		    EXTRA_DEP="--build-dep-resolver=aptitude --add-depends='golang-go (>= 2:1.16)'"
+	    fi
+		if [ "buster" = "${SUITE}" ]; then
+            # https://packages.debian.org/buster/golang-go
+		    EXTRA_DEP="--build-dep-resolver=aptitude --add-depends='golang-go (>= 2:1.12)'"
+	    fi
+    fi
 
 	for PACKAGE_URL_BRANCH in ${PACKAGE_URL_LIST}; do
 		PACKAGE_URL=$(echo ${PACKAGE_URL_BRANCH} | cut -d '|' -f 1)
@@ -57,7 +70,7 @@ for DISTRO_SUITE in ${DISTRO_SUITE_LIST}; do
 		uscan --download-current-version
 		dch --force-distribution -m -D ${SUITE} -l "+${SUITE}+" ${SUITE}
 		git diff 
-		sbuild -d ${SUITE} --no-run-lintian --extra-package ../ || exit 1
+		sbuild -d ${SUITE} --no-run-lintian --extra-package ../ ${EXTRA_DEP} || exit 1
 		git checkout -- .
 	done
 
