37b95caa08eef739a7704b5a91ec8a22c097221b
François Kooman <fkooman@tuxed.net>
2022-04-21 11:30:05 +0200

    recreate full repo on every build run

--- eduvpn_v3-dev.sh
+++ eduvpn_v3-dev.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 REPO_DIR=${HOME}/repos/$(basename ${0} .sh)
-mkdir -p ${REPO_DIR}/conf
-cp $(basename ${0} .sh).distributions ${REPO_DIR}/conf/distributions
+rm -rf "${REPO_DIR}"
+mkdir -p "${REPO_DIR}/conf"
+cp $(basename ${0} .sh).distributions "${REPO_DIR}/conf/distributions"
 
 DISTRO_SUITE_LIST="
 	debian|bullseye
@@ -24,30 +25,24 @@ for DISTRO_SUITE in ${DISTRO_SUITE_LIST}; do
 	DISTRO=$(echo ${DISTRO_SUITE} | cut -d '|' -f 1)
 	SUITE=$(echo ${DISTRO_SUITE} | cut -d '|' -f 2)
 	for PACKAGE_URL_BRANCH in ${PACKAGE_URL_LIST}; do
+		cd "${TMP_DIR}"
 		PACKAGE_URL=$(echo ${PACKAGE_URL_BRANCH} | cut -d '|' -f 1)
 		PACKAGE_BRANCH=$(echo ${PACKAGE_URL_BRANCH} | cut -d '|' -f 2)
-		cd ${TMP_DIR}
 		PACKAGE_NAME=$(basename ${PACKAGE_URL})
-		echo ${PACKAGE_NAME}
-		git clone -b ${PACKAGE_BRANCH} ${PACKAGE_URL}
-		cd ${PACKAGE_NAME} || exit
+		git clone -b "${PACKAGE_BRANCH}" "${PACKAGE_URL}" || exit 1
+		cd "${PACKAGE_NAME}" || exit 1
 		uscan --overwrite-download --download-current-version
-		dch --force-distribution -m -D ${SUITE} -l "+${SUITE}+" ${SUITE}
-		git diff 
-		sbuild -d ${SUITE} --no-run-lintian --extra-package ../ || exit 1
+		dch --force-distribution -m -D "${SUITE}" -l "+${SUITE}+" "${SUITE}"
+		sbuild \
+			-d "${SUITE}" \
+			--no-run-lintian \
+			--extra-package ../ || exit 1
+
 		git checkout -- .
 	done
 
-	# binaries
 	for PACKAGE in ${TMP_DIR}/*${SUITE}*.deb; do
-		reprepro -b ${REPO_DIR} includedeb ${SUITE} "${PACKAGE}" || true
-	done
-
-	# sources
-	for PACKAGE in ${TMP_DIR}/*${SUITE}*.dsc; do
-		reprepro -b ${REPO_DIR} includedsc ${SUITE} "${PACKAGE}" || true
+		echo "Adding ${PACKAGE}..."
+		reprepro -b "${REPO_DIR}" includedeb "${SUITE}" "${PACKAGE}"
 	done
 done
-
-echo "*** DONE ***"
-echo "Result in: ${TMP_DIR}"
