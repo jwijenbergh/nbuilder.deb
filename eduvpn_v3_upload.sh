f406e56fe8ce8c8e504f14946ba8acea7c57be45
François Kooman <fkooman@tuxed.net>
2022-05-18 10:29:17 +0200

    add "known_hosts" format for server fingerprint

--- eduvpn_v3_upload.sh
+++ eduvpn_v3_upload.sh
@@ -2,6 +2,10 @@
 
 REPO_DIR=${HOME}/repos/eduvpn_v3
 
+#ams-cdn.eduvpn.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9xsoHonghdxsMFzlSBRpF+ZWEWlPselk7S3AQ4LAbw
+#tromso-cdn.eduroam.no ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGnl+83/iCGbibgmsuDzDMunX1B6hDAEEvk3eKl3gljp
+#ifi2-cdn.eduroam.no ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1f3xpRfaOUmtIvs+x/v2GlzGKHQDmLiscRkZqQd931
+
 SERVER_LIST="
 	eduvpn-repo@tromso-cdn.eduroam.no
 	eduvpn-repo@ifi2-cdn.eduroam.no
